39717c9993ea65407dade139e28bdbc1bb667b61
TransportesMundiales <138054879+TransportesMundiales@users.noreply.github.com>
2023-10-21 12:04:50 -0600

    Eslint Configurado

--- 01-vite-vanilla/src/components/holamundo.js
+++ 01-vite-vanilla/src/components/holamundo.js
@@ -1,4 +1,4 @@
-const holaMundo = () => 
+const holaMundo = () =>
     `<h1> HOLA MUNDO VITE JS VANILLA </h1> 
     <p> CONTENIDO DESDE UNA TEMPLATE STRING </p>
     `
--- 01-vite-vanilla/src/main.js
+++ 01-vite-vanilla/src/main.js
@@ -1,11 +1,6 @@
 import './styles/style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from '/vite.svg'
 import holaMundo from './components/holamundo'
-
 
 document.querySelector('#app').innerHTML = `
   ${holaMundo()}
 `
-
-
